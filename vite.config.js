8c6a8c0b3ce2e2eb633028430794332a82993547
rui <528987496l@gmail.com>
2024-09-04 15:31:32 +0800

    删除冗余引用

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import obfuscator from 'rollup-plugin-obfuscator';
 import path from 'path';
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
